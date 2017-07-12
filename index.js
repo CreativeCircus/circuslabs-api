f1836e36cc6a2755bf662160e269e8d0447c707a
chrissilich <chrissilich@gmail.com>
2017-07-12 18:08:54 -0400

    Version 1.0 done and documented

---
+++ index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+var cors = require('cors')
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded())
+app.use(cors())
+
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+
+var Item = mongoose.model('Item', { name: String, type: String, content: String });
+
+
+app.get('/', function (req, res) {
+	res.send('Hello World!')
+})
+
+app.get('/data/:name', function (req, res) {
+
+	Item.findOne({ name: req.params.name }, function(err, item) {
+		if (err) {
+			console.error("error")
+			res.status(500).send("error")
+		} else if (!item) {
+			res.status(404).send({message: "Could not find data by that name"})
+		} else {
+			console.log(item);
+			res.status(200).send(item.content)
+		}
+	});
+})
+
+
+app.post('/data/:name', function (req, res) {
+	
+	Item.findOne({ name: req.params.name }, function(err, item) {
+		if (err) {
+			console.error("error")
+			res.status(500).send("error")
+			return
+		}
+		if (!item) {
+			item = new Item();
+			item.name = req.params.name;
+		}
+	
+		if (!req.body.type) {
+			res.status(400).send({message: "You must provide a data type"});
+			return;
+		} else if (req.body.type != "number" && req.body.type != "string") {
+			res.status(400).send({message: "You must provide a valid data type of 'string' or 'number' "});
+			return;
+		} else if (req.body.type == "number") {
+			// res.send(item.content)
+			// return
+			if (isNaN(item.content) || !item.content || item.content == "NaN") {
+				// res.send(item.content)
+				// return
+				item.content = 0;
+			}
+			if (!req.body.action) {
+				res.status(400).send({message: "For numbers, you must provide an action: '++', '--', '+=', or '=' "});
+				return
+			} else if (req.body.action == "++") {
+				item.content++;
+			} else if (req.body.action == "--") {
+				item.content--;
+			} else if (req.body.action == "+=") {
+				if (!req.body.quantity) {
+					res.status(400).send({message: "For numbers, with action '+=', you have to provide a quantity"});
+					return
+				}
+				item.content -= -req.body.quantity;
+			} else if (req.body.action == "=") {
+				if (!req.body.quantity) {
+					res.status(400).send({message: "For numbers, with action '=', you have to provide a quantity"});
+					return
+				}
+				item.content = req.body.quantity;
+			}
+			
+		} else if (req.body.type == "string") {
+			
+			if (!req.body.content) {
+				res.status(400).send({message: "For strings, you have to provide a 'content' property"});
+				return
+			} else if (req.body.content.length > 4096) {
+				res.status(400).send({message: "Strings have a max length of 4096 characters. Yours was " + req.body.content.length + " characters long."});
+				return
+			}
+			item.content = req.body.content;
+			
+		}
+
+		item.save()
+		res.send(item.content)
+	})
+})
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!')
+})
---
+++ readme.md
@@ -0,0 +1,75 @@
+# How to use the Circus General Purpose API v1.0.0
+
+## URLs
+Construct your URLs like this:
+`http://circuslabs.net:3000/data/your-key-here`
+The same URL is used for getting and setting data. Replace `your-key-here` with whatever name you want your data to be stored in. Make it something that is unlikely to be used by another student.
+
+## Sending data to the API
+Do a POST request for the above URL, with a data object describing the data you want to store or update. If data already exists, it will be overwritten or modified. If it doesn't, it will be created. In the case of numbers, they default to 0.
+
+```javascript
+$.ajax("http://circuslabs.net:3000/data/example-key-12345", {
+	method: "POST",
+	data: {
+		type: "string",
+		content: "Hello World"
+	}
+}).done(function(data, textStatus, jqXHR) {
+	console.log(data);
+}).fail(function(jqXHR, textStatus, errorThrown) {
+	console.warn(textStatus);
+})
+```
+
+Inside the data object, you need to specify a `type`. Valid types are `string` and `number`. 
+
+If you are storing a `string`, you also need to provide a `content` property, with the string you want to store.
+```javascript
+{
+	type: "string",
+	content: "Hello World"
+}
+```
+
+If you are storing a `number`, you also need to provide an `action` property, with action you want to perform on the number, and sometimes a `quantity` property, with the amount you want to perform said action. See the examples if this doesn't make sense.
+```javascript
+{
+	type: "number",
+	action: "++" // increases the number by 1
+}
+```
+```javascript
+{
+	type: "number",
+	action: "--" // decreases the number by 1
+}
+```
+```javascript
+{
+	type: "number",
+	action: "+=", // increases the number by the quantity below
+	quantity: 5
+}
+```
+```javascript
+{
+	type: "number",
+	action: "=", // sets the number to equal the quantity below
+	quantity: 5
+}
+```
+
+
+## Getting data from the API
+Do a GET request for the above URL, with a data object describing the data you want to store/update.
+
+```javascript
+$.ajax("http://circuslabs.net:3000/data/example-key-12345", {
+	method: "GET"
+}).done(function(data, textStatus, jqXHR) {
+	console.log(data);
+}).fail(function(jqXHR, textStatus, errorThrown) {
+	console.warn(textStatus);
+})
+```
